55905424d6d032008f0dd7cec1e5a08c42ebba78
Sebastian <sebastiancamilomarin@gmail.com>
2018-05-09 17:54:39 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -4,8 +4,8 @@ get '/' do
    @nombre=params[:nombre].capitalize
     
    if @nombre == @nombre  || @nombre!= 0
-       " <h1> Hola #{@nombre}!</h1>"
+        <h1> "Hola #{@nombre}!"</h1>
              else
-         "<h1> Hola deconocido!</h1>"
+         <h1> "Hola deconocido!"</h1>
    end
 end
